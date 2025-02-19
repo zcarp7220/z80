ffa9fcfb0689cb344f22c780170c09afc4a1f749
zcarp <zcarp7220@gmail.com>
2025-02-18 21:57:43 -0600

    Refacotr CPU defintion

--- src/z80.c
+++ src/z80.c
@@ -1,9 +1,6 @@
 #include "z80.h"
 struct cpu z80;
 void step(u_int8_t opcode[3]) {
-  BCOperatons(true);
-  DEOperatons(true);
-  HLOperatons(true);
   if (opcode[0] == 0xCB) {
     switch (opcode[0]) {}
   } else {
@@ -13,49 +10,24 @@ void step(u_int8_t opcode[3]) {
       break;
     case 0x01:
       z80.BC = opcode[1] | opcode[2] << 8;
-      BCOperatons(true);
       z80.PC += 3;
       break;
     case 0x02:
-      z80.A = loadMemory(z80.BC);
+      z80.A = memRead(z80.BC);
       z80.PC += 2;
       break;
     case 0x03:
       z80.BC++;
-      BCOperatons(false);
       z80.PC += 1;
       break;
     case 0x04:
       z80.B++;
       z80.PC += 1;
       break;
+    case 0x05:
+      z80.B--;
+      z80.PC += 1;
+      break;
     }
   }
-}
-void BCOperatons(bool BCMerge) {
-  if (BCMerge) {
-    z80.BC = z80.B << 8 | z80.C;
-  } else {
-    z80.B = z80.BC & 0xF0 >> 0xF;
-    z80.C = z80.BC & 0x0F;
-  }
-  return;
-}
-void DEOperatons(bool DEMerge) {
-  if (BCMerge) {
-    z80.DE = z80.D << 8 | z80.E;
-  } else {
-    z80.D = z80.DE & 0xF0 >> 0xF;
-    z80.E = z80.DE & 0x0F;
-  }
-  return;
-}
-void HLOperatons(bool HLMerge) {
-  if (BCMerge) {
-    z80.HL = z80.H << 8 | z80.L;
-  } else {
-    z80.H = z80.HL & 0xF0 >> 0xF;
-    z80.L = z80.HL & 0x0F;
-  }
-  return;
 }
--- src/z80.h
+++ src/z80.h
@@ -1,22 +1,21 @@
 #include "common.h"
 void step(u_int8_t opcode[3]);
-bool halt;
 struct cpu{
-u_int8_t A;
-u_int8_t F;
-u_int8_t B;
-u_int8_t D;
-u_int8_t H;
-u_int8_t C;
-u_int8_t E;
-u_int8_t L;
-u_int16_t BC;
-u_int16_t DE;
-u_int16_t HL;
-u_int16_t PC;
-u_int16_t SP;
-u_int16_t IX;
-u_int16_t IY;
-u_int16_t AF;
-u_int8_t R;
+
+bool halt;
+//Registers
+struct{u_int8_t A; u_int8_t F;};
+union{struct{u_int8_t B; u_int8_t C;};u_int16_t BC; };
+union{struct{u_int8_t D; u_int8_t E;};u_int16_t DE; };
+union{struct{u_int8_t H; u_int8_t L;};u_int16_t HL; };
+//Shaow Registers
+struct{u_int8_t Ap; u_int8_t Fp;};
+union{struct{u_int8_t Bp; u_int8_t Cp;};u_int16_t BCp; };
+union{struct{u_int8_t Dp; u_int8_t Ep;};u_int16_t DEp; };
+union{struct{u_int8_t Hp; u_int8_t Lp;};u_int16_t HLp; };
+//Speacal Pourpse Registers
+u_int16_t PC; u_int16_t SP; u_int16_t IX; 
+u_int16_t IY; u_int16_t I; u_int16_t R;
+
 };
+
