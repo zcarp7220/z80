39f9cb652d8539791c20eb9545c4bfcc9b63254c
zcarp <zcarp7220@gmail.com>
2025-02-19 09:38:37 -0600

    change types to be the standard

--- src/z80.c
+++ src/z80.c
@@ -1,33 +1,3 @@
 #include "z80.h"
 struct cpu z80;
-void step(u_int8_t opcode[3]) {
-  if (opcode[0] == 0xCB) {
-    switch (opcode[0]) {}
-  } else {
-    switch (opcode[0]) {
-    case 0x0:
-      z80.PC += 1;
-      break;
-    case 0x01:
-      z80.BC = opcode[1] | opcode[2] << 8;
-      z80.PC += 3;
-      break;
-    case 0x02:
-      z80.A = memRead(z80.BC);
-      z80.PC += 2;
-      break;
-    case 0x03:
-      z80.BC++;
-      z80.PC += 1;
-      break;
-    case 0x04:
-      z80.B++;
-      z80.PC += 1;
-      break;
-    case 0x05:
-      z80.B--;
-      z80.PC += 1;
-      break;
-    }
-  }
-}
+void step() {}
--- src/z80.h
+++ src/z80.h
@@ -1,21 +1,20 @@
 #include "common.h"
-void step(u_int8_t opcode[3]);
+void step(uint8_t opcode[3]);
 struct cpu{
 
 bool halt;
 //Registers
-struct{u_int8_t A; u_int8_t F;};
-union{struct{u_int8_t B; u_int8_t C;};u_int16_t BC; };
-union{struct{u_int8_t D; u_int8_t E;};u_int16_t DE; };
-union{struct{u_int8_t H; u_int8_t L;};u_int16_t HL; };
+struct{uint8_t A; uint8_t F;};
+union{struct{uint8_t B; uint8_t C;};uint16_t BC; };
+union{struct{uint8_t D; uint8_t E;};uint16_t DE; };
+union{struct{uint8_t H; uint8_t L;};uint16_t HL; };
 //Shaow Registers
-struct{u_int8_t Ap; u_int8_t Fp;};
-union{struct{u_int8_t Bp; u_int8_t Cp;};u_int16_t BCp; };
-union{struct{u_int8_t Dp; u_int8_t Ep;};u_int16_t DEp; };
-union{struct{u_int8_t Hp; u_int8_t Lp;};u_int16_t HLp; };
+struct{uint8_t Ap; uint8_t Fp;};
+union{struct{uint8_t Bp; uint8_t Cp;};uint16_t BCp; };
+union{struct{uint8_t Dp; uint8_t Ep;};uint16_t DEp; };
+union{struct{uint8_t Hp; uint8_t Lp;};uint16_t HLp; };
 //Speacal Pourpse Registers
-u_int16_t PC; u_int16_t SP; u_int16_t IX; 
-u_int16_t IY; u_int16_t I; u_int16_t R;
+uint16_t PC; uint16_t SP; uint16_t IX; 
+uint16_t IY; uint16_t I; uint16_t R;
 
 };
-
